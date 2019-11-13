e75807a31435e4d808b3ecd8f6c0aa3c4f45d085
Mikheull <mikhael.bailly1@orange.fr>
2019-11-13 19:50:43 +0100

    separate first-connection and re-connection views, add logout url

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,12 @@ router.use('/', require('./index') );
 router.use('/register', require('./register') );
 router.use('/login', require('./login') );
 router.use('/reset-password', require('./password') );
+router.get('/logout', function (req, res) {
+	res.cookie('logged', 'null', {maxAge: 0});
+	res.clearCookie('logged');
+	res.redirect('/login');
+})
+
 router.use('/map', require('./map') );
 router.use('/questions', require('./questions') );
 router.use('/account', require('./account') );
