bc4305522f46db6545b399ccd1abf0bb2cf15184
HOUAYS Yassine <Yass76300@gmail.com>
2019-11-14 16:51:03 +0100

    ajout icone + modif home

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/', async function(req, res, next) {
 			assets: {
 				stylesheets: ['home'],
 				online_stylesheets: [],
-				javascripts: [],
+				javascripts: ['home'],
 				online_javascripts: [],
 				libs: ['jquery', 'fontawesome', 'feather-icons']
 			}
