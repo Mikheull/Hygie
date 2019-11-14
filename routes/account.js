4313cf4488d91b2919d73e19f5a542836795f796
Mikheull <mikhael.bailly1@orange.fr>
2019-11-14 16:50:57 +0100

    update account

--- routes/account.js
+++ routes/account.js
@@ -42,8 +42,8 @@ router.get('/', async function(req, res, next) {
 });
 
 
-/* POST Add antecedent page. */
 router.post('/', async function(req, res) {
+	/* POST Add antecedent page. */
 	if(req.body.save_btn == ''){
 		
 		if(req.body.reason !== '' && req.body.reason){
@@ -60,6 +60,57 @@ router.post('/', async function(req, res) {
 			
 		}else{
 			console.log('erreur = incomplet');
+		}
+
+	/* POST save account. */
+	}else if(req.body.save_compte_btn == ''){
+			
+		let first_name = (req.body.first_name !== '' && req.body.first_name) ? req.body.first_name : null ;
+		let lastname = (req.body.lastname !== '' && req.body.lastname) ? req.body.lastname : null ;
+		let mail = (req.body.mail !== '' && req.body.mail) ? req.body.mail : null ;
+		let telephone = (req.body.telephone !== '' && req.body.telephone) ? req.body.telephone : null ;
+		let adress = (req.body.adress !== '' && req.body.adress) ? req.body.adress : null ;
+		let birdthay = (req.body.birdthay !== '' && req.body.birdthay) ? req.body.birdthay : null ;
+			
+		let request = await account_obj.updateAccount_main(req.myID, first_name, lastname, mail, telephone, adress, birdthay);
+		if(request !== false){
+			res.redirect('account');
+		}else{
+			res.send('erreur lors de l\'update ');
+		}
+
+	/* POST save general account. */
+	}else if(req.body.save_general_btn == ''){
+		
+		let gender = (req.body.gender !== '' && req.body.gender) ? req.body.gender : null ;
+		let height = (req.body.height !== '' && req.body.height) ? req.body.height : null ;
+		let weight = (req.body.weight !== '' && req.body.weight) ? req.body.weight : null ;
+		let blood_group = (req.body.blood_group !== '' && req.body.blood_group) ? req.body.blood_group : null ;
+			
+		let request = await account_obj.updateAccount_gen(req.myID, gender, height, weight, blood_group);
+		if(request !== false){
+			res.redirect('account');
+		}else{
+			res.send('erreur lors de l\'update ');
+		}
+
+	/* POST save password account. */
+	}else if(req.body.save_pass_btn == ''){
+			
+		let password = (req.body.password !== '' && req.body.password) ? req.body.password : null ;
+		let confirm_password = (req.body.confirm_password !== '' && req.body.confirm_password) ? req.body.confirm_password : null ;
+			
+		if (password == confirm_password){
+
+			let request = await account_obj.updateAccount_pass(req.myID, password);
+			if(request !== false){
+				res.redirect('logout');
+			}else{
+				res.send('erreur lors de l\'update ');
+			}
+
+		}else{
+			res.send('mot de passe différent');
 		}
 
 	}
